4ed8a4610fd6d380635d9cb49b1cadebcbd6744a
xstrnad2 <514456@mail.muni.cz>
2022-09-26 10:20:22 -0400

    lab8

--- cs330/Labs/Lab08Click.js
+++ cs330/Labs/Lab08Click.js
@@ -17,12 +17,20 @@ function init() {
     if (!gl) alert("WebGL 2.0 isn't available");
 
     // 2 - listener event for the button which toggles whether to save the points clicked
-    
+    var savePoints = document.getElementById("Button1");
+    savePoints.addEventListener("click", function(){
+        savePoints = !savePoints;
+        points = [vec2(  0.00 ,  0.00 )];
+        render();
+        })
 
     // 1 - listener event for the mouse click
     // 3 - will later need to be updated in conjunction with whether the points are saved
     canvas.addEventListener("mousedown", function(event){
-       t  = 
+       if (!savePoints){
+        points=[vec2(  0.00 ,  0.00 )];
+       }
+        t  = vec2((2*event.clientX)/canvas.width-1, 2*(canvas.height-event.clientY)/canvas.height - 1);
        points.push(t);
        render();
     });
