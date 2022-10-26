f77c8aa54d51fd3887efd5d94725db522129eca6
xstrnad2 <514456@mail.muni.cz>
2022-10-26 10:42:41 -0400

    Update Lab15ShadedCube.js

--- cs330/Labs/Lab15ShadedCube.js
+++ cs330/Labs/Lab15ShadedCube.js
@@ -27,12 +27,12 @@ var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
 var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
 // original
-//
+/*
 var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
 var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
 var materialSpecular = vec4(1.0, 0.8, 0.0, 1.0);
 var materialShininess = 100.0;
-//
+*/
 
 // black plastic
 /*
@@ -99,12 +99,12 @@ var materialShininess = 51.2;
 */
 
 // polished silver
-/*
+//
 var materialAmbient  = vec4(0.23125 , 0.23125 , 0.23125 , 1.0);
 var materialDiffuse  = vec4(0.2775 , 0.2775 , 0.2775 , 1.0);
 var materialSpecular  = vec4(0.773911 , 0.773911 , 0.773911 , 1.0);
 var materialShininess = 89.6;
-*/
+//
 
 var ctm;
 var ambientColor, diffuseColor, specularColor;
