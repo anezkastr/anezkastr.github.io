a8af267b41791ee2e116aed70638a6abd75b1b24
xstrnad2 <514456@mail.muni.cz>
2022-11-14 10:03:37 -0500

    lab19

--- cs330/Labs/Lab19TextureImage.js
+++ cs330/Labs/Lab19TextureImage.js
@@ -53,10 +53,20 @@ var vertexColors = [
 ];
 
 window.onload = init;
-
+var image = document.getElementById("texImage");
+configureTexture(image);
 function configureTexture( image ) {
-
-}
+    texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB,
+    gl.RGB, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+    gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER,
+   gl.NEAREST);
+    gl.uniform1i(gl.getUniformLocation(program, "uTextureMap"), 0);
+   }
 
 function triangle (a,b,c,triNum)
 {
